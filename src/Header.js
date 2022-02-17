5688cd441f51922a5d8f1bf576738c6f84f18025
Fatih <95652578+fatihqaz@users.noreply.github.com>
2022-02-17 03:48:51 +0300

    navlink

--- src/Header.js
+++ src/Header.js
@@ -1,17 +1,26 @@
-import {Link} from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 export default function Header() {
   return (
     <div className="App-header">
-  
-    <div className="Nav" >
-       <Link className="App-link" to="ders1">Ders 1</Link>
-       <Link className="App-link" to="ders2">Ders 2</Link>
-       <Link className="App-link" to="ders3">Ders 3</Link>
-       <Link className="App-link" to="ders4">Ders 4</Link>
-       <Link className="App-link" to="ders5">Ders 5</Link>
+      <div className="Nav" >
+
+        <NavLink className={({ isActive }) =>
+          isActive ? "activeClassName" : "App-link"} to="ders1">Ders 1</NavLink>
+
+        <NavLink className={({ isActive }) =>
+          isActive ? "activeClassName" : "App-link"} to="ders2">Ders 2</NavLink>
+
+        <NavLink className={({ isActive }) =>
+          isActive ? "activeClassName" : "App-link"} to="ders3">Ders 3</NavLink>
+
+        <NavLink className={({ isActive }) =>
+          isActive ? "activeClassName" : "App-link"} to="ders4">Ders 4</NavLink>
+
+        <NavLink className={({ isActive }) =>
+          isActive ? "activeClassName" : "App-link"} to="ders5">Ders 5</NavLink>
+
+      </div>
     </div>
-    
- </div>
   )
 }
