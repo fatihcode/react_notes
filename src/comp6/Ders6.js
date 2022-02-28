1e295d606d959396539481b56fa5ab1a2083cbc3
Fatih <95652578+fatihqaz@users.noreply.github.com>
2022-03-01 01:40:42 +0300

    new lesson

--- src/comp6/Ders6.js
+++ src/comp6/Ders6.js
@@ -1,10 +1,61 @@
 import React from 'react'
 
+import { createStore, combineReducers } from 'redux'
+
+function reducer(state,action) { 
+  console.log(state,action);
+
+  if (action.type=='changeTheState') {   
+    return action.payload
+  }
+  return 'Old State'
+}
+
+const store = createStore(reducer)
+
+console.log(store.getState());
+
+const action = {
+  type: 'changeTheState',
+  payload: { newState: 'New State' }
+}
+
+store.subscribe(()=>{
+  console.log(store.getState());
+})
+
+store.dispatch(action)
+
+//------------------------------------------
+
+function userReducer(state="",action) {
+  return state
+}
+
+function jobReducer(state=[],action) {
+  return state
+}
+
+const rootReducer=combineReducers(
+  {userReducer,jobReducer}
+)
+
+const myStore = createStore(rootReducer,
+  {userReducer:'Ahmet',jobReducer:["Dev","Op"]})
+
+myStore.subscribe(()=>{
+  console.log(myStore.getState());
+})
+
+console.log(myStore.getState());
+
+//------------------------------------------
+
 export default function Ders6() {
 
   return (
     <div>
-      <h2>[Ders 6] - </h2>
+      <h2>[Ders 6] - Redux</h2>
      
 
     </div>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+// -----------------------------------
+
+
+
+
+// -----------------------------------
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
